d0fcf670e4e27f9dba405c0a0b0ed64a092b323b
wenner <wennersannerma@gmail.com>
2022-01-12 09:37:52 -0300

    CRUD completo do projeto

---
+++ src/test/java/br/com/dicasdeumdev/api/ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.dicasdeumdev.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void main() {
+        ApiApplication.main(new String[] {});
+    }
+
+}
