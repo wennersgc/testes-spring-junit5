d2f772d63c5ef4786c6e10b77c00770fc1184c5b
wenner <wennersannerma@gmail.com>
2022-01-12 12:14:17 -0300

    Teste para a classe user service metodo create com execação se já houver e-mail cadastrado

--- src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -3,7 +3,9 @@ package br.com.dicasdeumdev.api.services.impl;
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
+import br.com.dicasdeumdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,8 +18,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -30,6 +34,7 @@ class UserServiceImplTest {
     public static final String PASSWORD = "123";
     public static final String OBJETO_NÃO_ENCONTRADO = "Objeto não encontrado";
     public static final int INDEX = 0;
+    public static final String E_MAIL_JÁ_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
 
     @InjectMocks
     private UserServiceImpl service;
@@ -66,9 +71,9 @@ class UserServiceImplTest {
     void deveRetonarObjectNotFoundException_aoBuscarPorID_eUsuarioNaoExistir() {
         when(repository.findById(ID)).thenThrow(new ObjectNotFoundException(OBJETO_NÃO_ENCONTRADO));
 
-        Exception exception = assertThrows(ObjectNotFoundException.class, () -> repository.findById(ID));
+        Exception exception = assertThrows(ObjectNotFoundException.class, () -> service.findById(ID));
 
-        assertThrows(ObjectNotFoundException.class, () -> repository.findById(ID));
+        assertThrows(ObjectNotFoundException.class, () -> service.findById(ID));
         assertTrue(exception.getMessage().contains(OBJETO_NÃO_ENCONTRADO));
         assertEquals(ObjectNotFoundException.class, exception.getClass());
     }
@@ -96,6 +101,17 @@ class UserServiceImplTest {
         assertEquals(User.class, response.getClass());
 
         verificaAtributos(response);
+    }
+
+    @Test
+    void deveLancarDataIntegrityViolationException_aoCriarUmUsuario() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+        optionalUser.get().setId(2);
+
+        Exception ex = assertThrows(DataIntegratyViolationException.class, () -> service.create(userDTO));
+
+        assertEquals(DataIntegratyViolationException.class, ex.getClass());
+        assertTrue(ex.getMessage().contains(E_MAIL_JÁ_CADASTRADO_NO_SISTEMA));
     }
 
     private void verificaAtributos(User response) {
